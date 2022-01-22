9adbc6e71cfa529753a7441e0f1830cd9b3778ae
André Augusto dos Santos <andreaugustodossantos@gmail.com>
2022-01-22 17:45:09 -0300

    gv1

---
+++ nosso role/script/script.js
@@ -0,0 +1,10 @@
+var fila = document.getElementById("lista");
+var gole = document.getElementById("lista2");
+
+function descobrir (){
+
+if (fila.value % 2 === 0 && gole.value % 2 === 0){
+   
+}
+res.innerHTML('Você ganhou um beijo na boca');
+}
