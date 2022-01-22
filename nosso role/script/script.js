5ead1023c2fea58b78b8c4a929d41cb05f40fbfe
André Augusto dos Santos <andreaugustodossantos@gmail.com>
2022-01-22 18:49:05 -0300

    vs3

--- nosso role/script/script.js
+++ nosso role/script/script.js
@@ -1,10 +1,13 @@
+function descobrir(){
 var fila = document.getElementById("lista");
 var gole = document.getElementById("lista2");
+var value1 = fila.options[fila.selectedIndex].value;
+var value2 = gole.options[gole.selectedIndex].value;
 
-function descobrir (){
 
-if (fila.value % 2 === 0 && gole.value % 2 === 0){
-   
+if (value1 % 2 == 0 && value2 % 2 == 0){
+   alert('Você ganhou um beijo na boca');
+    
 }
-res.innerHTML('Você ganhou um beijo na boca');
+res.innerHTML('Você Ganhou um Beijo na Boca')
 }
